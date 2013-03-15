9a5d4c5eec4084244d58e0199c4e3edaf089028e
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-03-15 11:28:10 +0100

    Added Windows configuration scripts

---
+++ windows/configure-windows.bat
@@ -0,0 +1,7 @@
+
+setx HOME "E:"
+setx -m ANT_HOME    "%%MYPROGRAMFILES%%\Ant"
+setx -m CYGWIN_HOME "C:\cygwin"
+setx -m FLEX_HOME   "C:\flex"
+setx -m MAVEN_HOME  "%%MYPROGRAMFILES%%\Maven"
+setx -m PHP_HOME    "%%MYPROGRAMFILES%%\Php"
