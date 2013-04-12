f7d36d1b8e26afdeb8b263d4be03c6db943e39cd
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-04-12 10:48:03 +0200

    vcd script 1.0.1

--- src/vcloud-director/vcloud-director-content-type-fix.user.js
+++ src/vcloud-director/vcloud-director-content-type-fix.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          vCloud Director 5.1.1 Content-Type Workaround
-// @version       1.0.0
+// @version       1.0.1
 // @description	  Fixes the URL /loginComplete being served without any content-type
 // @author        Alexandre Mercier (pro.alexandre.mercier@gmail.com)
 // @namespace     http://github.com/amercier/
@@ -9,12 +9,15 @@
 // Notes:
 //   * is a wildcard character
 //   .tld is magic that matches all top-level domains (e.g. .com, .co.uk, .us, 
-var pres = window.document.getElementsByTagName('pre');
-if(pres.length > 0) {
-	console.warn('vCloud Director 5.1.1 Content-Type Workaround: replacing text with code');
-	var code = window.document.getElementsByTagName('pre')[0].childNodes[0].nodeValue.replace(/.*<script[^>]*>(.*)<\/script>.*/, '$1');
-	code = 'setTimeout(function() { ' + code + ' }, 50);';
-	var s = document.createElement('script');
-	s.innerHTML = code;
-	document.body.appendChild(s);
-}
+
+//window.onload = window.onload || function() {
+	var pres = window.document.getElementsByTagName('pre');
+	if(pres.length > 0) {
+		console.warn('vCloud Director 5.1.1 Content-Type Workaround: replacing text with code');
+		var code = window.document.getElementsByTagName('pre')[0].childNodes[0].nodeValue.replace(/.*<script[^>]*>(.*)<\/script>.*/, '$1');
+		code = 'setTimeout(function() { ' + code + ' }, 50);';
+		var s = document.createElement('script');
+		s.innerHTML = code;
+		document.body.appendChild(s);
+	}
+//}
