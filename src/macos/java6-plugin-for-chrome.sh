70a799de877bcec95e6e33db52286688eb28a1e0
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-04-29 16:51:14 +0200

    Added Java plugin installer for Chrome

---
+++ src/macos/java6-plugin-for-chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# http://support.apple.com/kb/HT5559
+
+sudo mkdir -p /Library/Internet\ Plug-Ins/disabled
+sudo mv /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin /Library/Internet\ Plug-Ins/disabled
+sudo ln -sf /System/Library/Java/Support/Deploy.bundle/Contents/Resources/JavaPlugin2_NPAPI.plugin /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin 
+sudo ln -sf /System/Library/Frameworks/JavaVM.framework/Commands/javaws /usr/bin/javaws
