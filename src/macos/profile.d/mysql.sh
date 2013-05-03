e159e11ef7dc756aef998ed5b27509c00622b924
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-05-03 15:09:21 +0200

    Fixed scripts

--- src/macos/profile.d/mysql.sh
+++ src/macos/profile.d/mysql.sh
@@ -1,1 +1,4 @@
-/etc/profile.d/mysql.sh
+#!/bin/bash
+MYSQL_HOME=/usr/local/mysql/bin
+PATH=$PATH:$MYSQL_HOME
+export PATH
--- src/macos/profile.d/terminal.sh
+++ src/macos/profile.d/terminal.sh
@@ -1,1 +1,8 @@
-/etc/profile.d/terminal.sh
+#!/bin/bash
+export TERM=xterm-color
+export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+export COLOR_NC='\e[0m' # No Color
+[ -f "$HOME/.bashrc" ]       && . "$HOME/.bashrc"
+[ -f "$HOME/.bash_aliases" ] && . "$HOME/.bash_aliases"
