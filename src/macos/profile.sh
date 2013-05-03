35c913a3176fcd8fb53c2913b0c9073bb7176db6
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-05-03 15:05:51 +0200

    Updated profile.sh

---
+++ src/macos/profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[ ! -d /etc/profile.d ] && sudo mkdir /etc/profile.d \
+&& echo "Created /etc/profile directory" \
+|| echo "/etc/profile.d already exists, skipping"
+
+grep -q profile.d /etc/profile \
+&& echo "/etc/profile is already up-to-date, skipping" \
+|| (echo '
+# Customization
+for PROFILE_SCRIPT in $( ls /etc/profile.d/*.sh ); do
+  . $PROFILE_SCRIPT
+done' | sudo tee -a /etc/profile && echo "Updated /etc/profile")
