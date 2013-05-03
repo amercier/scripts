a3af4645e5d5484c2fa8a6e40d7ed3fd92b34111
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-05-03 16:29:55 +0200

    Improved terminal colors

--- src/macos/profile.d/terminal.sh
+++ src/macos/profile.d/terminal.sh
@@ -2,7 +2,7 @@
 export TERM=xterm-color
 export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
 export CLICOLOR=1
-export LSCOLORS=ExFxCxDxBxegedabagacad
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 export COLOR_NC='\e[0m' # No Color
 [ -f "$HOME/.bashrc" ]       && . "$HOME/.bashrc"
 [ -f "$HOME/.bash_aliases" ] && . "$HOME/.bash_aliases"
