8e6efe540ec088c926a7ea013958a4f954b70aa5
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-16 00:00:07 -0700

    Fixed yeoman -> yo

--- src/macos/all.sh
+++ src/macos/all.sh
@@ -2,16 +2,20 @@
 
 SCRIPTS=https://raw.githubusercontent.com/amercier/scripts/master/src/macos
 
-curl -s -L $SCRIPTS/delete-dsstore-files.sh | bash
-curl -s -L $SCRIPTS/install/google-chrome.sh | bash
-curl -s -L $SCRIPTS/install/spectacle.sh | bash
-curl -s -L $SCRIPTS/install/sublime-text.sh | bash
+curl -s -L $SCRIPTS/tweaks.sh | bash
+if [ "$(defaults read com.apple.desktopservices DSDontWriteNetworkStores)" == "false" ]; then
+  curl -s -L $SCRIPTS/delete-dsstore-files.sh | bash
+fi
+
+curl -s -L $SCRIPTS/profile.sh | bash
 curl -s -L $SCRIPTS/profile.d/git-prompt-extras.sh | sudo tee /etc/profile.d/git-prompt-extras.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/git-prompt.sh | sudo tee /etc/profile.d/git-prompt.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/local.sh | sudo tee /etc/profile.d/local.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/terminal.sh | sudo tee /etc/profile.d/terminal.sh >/dev/null
-curl -s -L $SCRIPTS/profile.sh | bash
-curl -s -L $SCRIPTS/tweaks.sh | bash
+
+curl -s -L $SCRIPTS/install/google-chrome.sh | bash
+curl -s -L $SCRIPTS/install/spectacle.sh | bash
+curl -s -L $SCRIPTS/install/sublime-text.sh | bash
 
 # HomeBrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -22,7 +26,7 @@ brew install git
 # node.js
 brew install nodejs
 chown -R "$(whoami):admin" /usr/local/lib/node_modules
-for package in gulp grunt bower yeoman; do
+for package in gulp grunt bower yo; do
   [ -d /usr/local/lib/node_modules/$package ] \
     && echo $package is already installed \
     || npm install $package -g
