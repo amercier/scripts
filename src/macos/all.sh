af80953363195f6e4b620c2056c12de81fcbc40e
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-15 23:46:04 -0700

    Fixed all.sh executing obsolete scripts

--- src/macos/all.sh
+++ src/macos/all.sh
@@ -6,13 +6,10 @@ curl -s -L $SCRIPTS/delete-dsstore-files.sh | bash
 curl -s -L $SCRIPTS/install/google-chrome.sh | bash
 curl -s -L $SCRIPTS/install/spectacle.sh | bash
 curl -s -L $SCRIPTS/install/sublime-text.sh | bash
-curl -s -L $SCRIPTS/java6-plugin-for-chrome.sh | bash
-curl -s -L $SCRIPTS/keys.sh | bash
-curl -s -L $SCRIPTS/profile.d/git-prompt-extras.sh | bash
-curl -s -L $SCRIPTS/profile.d/git-prompt.sh | bash
-curl -s -L $SCRIPTS/profile.d/local.sh | bash
-curl -s -L $SCRIPTS/profile.d/mysql.sh | bash
-curl -s -L $SCRIPTS/profile.d/terminal.sh | bash
+curl -s -L $SCRIPTS/profile.d/git-prompt-extras.sh | sudo tee /etc/profile.d/git-prompt-extras.sh >/dev/null
+curl -s -L $SCRIPTS/profile.d/git-prompt.sh | sudo tee /etc/profile.d/git-prompt.sh >/dev/null
+curl -s -L $SCRIPTS/profile.d/local.sh | sudo tee /etc/profile.d/local.sh >/dev/null
+curl -s -L $SCRIPTS/profile.d/terminal.sh | sudo tee /etc/profile.d/terminal.sh >/dev/null
 curl -s -L $SCRIPTS/profile.sh | bash
 curl -s -L $SCRIPTS/tweaks.sh | bash
 
@@ -21,3 +18,12 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 # Git
 brew install git
+
+# node.js
+brew install nodejs
+chown -R "$(whoami):admin" /usr/local/lib/node_modules
+for package in gulp grunt bower yeoman; do
+  [ -d /usr/local/lib/node_modules/$package ] \
+    && echo $package is already installed \
+    || npm install $package -g
+done
