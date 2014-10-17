78997f0e7061f7d6b90b8a3760779ae520c2fd93
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-16 22:08:48 -0700

    Added nvm

--- src/macos/all.sh
+++ src/macos/all.sh
@@ -11,6 +11,7 @@ curl -s -L $SCRIPTS/profile.sh | bash
 curl -s -L $SCRIPTS/profile.d/git-prompt-extras.sh | sudo tee /etc/profile.d/git-prompt-extras.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/git-prompt.sh | sudo tee /etc/profile.d/git-prompt.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/local.sh | sudo tee /etc/profile.d/local.sh >/dev/null
+curl -s -L $SCRIPTS/profile.d/nvm.sh | sudo tee /etc/profile.d/nvm.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/terminal.sh | sudo tee /etc/profile.d/terminal.sh >/dev/null
 
 curl -s -L $SCRIPTS/install/google-chrome.sh | bash
@@ -24,10 +25,11 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install git
 
 # node.js
-brew install nodejs
-chown -R "$(whoami):admin" /usr/local/lib/node_modules
+brew install nvm
+nvm install $(curl -L -s http://nodejs.org/ | grep Current | egrep -o 'v[0-9]+\.[0-9]+\.[0-9]+')
+
 for package in gulp grunt bower yo; do
-  [ -d /usr/local/lib/node_modules/$package ] \
+  [ -d $NODE_PATH/$package ] \
     && echo $package is already installed \
     || npm install $package -g
 done
---
+++ src/macos/profile.d/nvm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
