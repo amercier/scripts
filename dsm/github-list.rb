7acb294cec6d7a3590f7059ed4b15466f40ba38a
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-18 02:24:20 -0700

    Improved error handling and refactored a bit

--- dsm/github-list.rb
+++ dsm/github-list.rb
@@ -3,7 +3,7 @@
 # List repositories of a given user and all its public organizations
 # on standard output using the GitHub API
 #
-#     github-list.rb username
+#     github-list.rb USERNAME
 #
 #     > username/repo1
 #     > username/repo2
@@ -12,12 +12,17 @@
 #     > organisation1/repo2
 #     > organisation2/repo3
 
-require 'open-uri'
+require 'base64'
 require 'json'
+require 'open-uri'
 
 GITHUB_API_VERSION = "v3"
 GITHUB_API_URL = "https://api.github.com/"
 GITHUB_API_ACCEPT = "application/vnd.github.#{GITHUB_API_VERSION}+json"
+GITHUB_API_HEADERS = {
+  'Accept' => GITHUB_API_ACCEPT#,
+  # TODO 'Authorization' => "Basic #{Base64.encode64('username:password')}"
+}
 
 # Check argument numbers
 if ARGV.count != 1
@@ -26,7 +31,7 @@ end
 username = ARGV[0]
 
 def display_repositories(entity)
-  repos = JSON.parse(open(entity['repos_url'], "Accept" => GITHUB_API_ACCEPT).read)
+  repos = JSON.parse(open(entity['repos_url'], GITHUB_API_HEADERS).read)
   repos.each do |repo|
     puts repo['full_name']
   end
@@ -35,16 +40,16 @@ end
 begin
 
   # Connect to GitHub API and get user organization url
-  urls = JSON.parse(open(GITHUB_API_URL, "Accept" => GITHUB_API_ACCEPT).read)
+  urls = JSON.parse(open(GITHUB_API_URL, GITHUB_API_HEADERS).read)
 
   # Fetch user and get his URLs
-  user = JSON.parse(open(urls['user_url'].sub('{user}', username), "Accept" => GITHUB_API_ACCEPT).read)
+  user = JSON.parse(open(urls['user_url'].sub('{user}', username), GITHUB_API_HEADERS).read)
 
   # User repositories
   display_repositories user
 
   # Fetch user and get his URLs
-  orgs = JSON.parse(open(user['organizations_url'], "Accept" => GITHUB_API_ACCEPT).read)
+  orgs = JSON.parse(open(user['organizations_url'], GITHUB_API_HEADERS).read)
 
   # Iterate over organizations
   orgs.each do |org|
@@ -53,6 +58,8 @@ begin
 
 rescue OpenURI::HTTPError => e
   case e.io.status[0]
+  when "403"
+    abort JSON.parse(e.io.string)['message']
   when "404"
     abort "Unknown user #{username}"
   else
