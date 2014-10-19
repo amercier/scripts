731d2d3f23b1f91786a4c0afcea958f08abf604a
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-18 19:58:40 -0700

    Fixed RVM and NVM

--- src/macos/all.sh
+++ src/macos/all.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash --login
 
 SCRIPTS=https://raw.githubusercontent.com/amercier/scripts/master/src/macos
 
@@ -15,6 +15,7 @@ curl -s -L $SCRIPTS/profile.d/local.sh | sudo tee /etc/profile.d/local.sh >/dev/
 curl -s -L $SCRIPTS/profile.d/nvm.sh | sudo tee /etc/profile.d/nvm.sh >/dev/null
 curl -s -L $SCRIPTS/profile.d/terminal.sh | sudo tee /etc/profile.d/terminal.sh >/dev/null
 curl -s -L $SCRIPTS/profile.sh | bash
+sudo chmod +x /etc/profile.d/*.sh
 
 curl -s -L $SCRIPTS/install/google-chrome.sh | bash
 curl -s -L $SCRIPTS/install/firefox.sh | bash
@@ -29,18 +30,18 @@ brew install git
 
 # node.js
 brew install nvm
-nvm install $(curl -L -s http://nodejs.org/ | grep Current | egrep -o 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
+node_version="$(curl -L -s http://nodejs.org/ | grep Current | egrep -o 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)"
+nvm --help >/dev/null 2>&1 && echo "NVM is already installed" || (nvm install $node_version && nvm --default use $node_version)
 
-for package in gulp grunt-cli bower yo generator-angular; do
-  [ -d $NODE_PATH/$package ] && echo $package is already installed || npm install $package -g
+for module in gulp grunt-cli bower yo generator-angular; do
+  [ -d $NVM_DIR/current/lib/node_modules/$module ] && echo $module is already installed || npm install $module -g
 done
 
 # Ruby
 which rvm >/dev/null && echo "RVM is already installed" || curl -sSL https://get.rvm.io | bash -s stable
 source ~/.profile
-rvm_version = "$(curl -s -L https://www.ruby-lang.org/en/downloads/ | grep stable | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)"
-rvm install $rvm_version
-rvm --default use $rvm_version
+ruby_version="$(curl -s -L https://www.ruby-lang.org/en/downloads/ | grep stable | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)"
+[ "$(rvm current)" == "ruby-$ruby_version" ] && echo "Ruby $ruby_version is already installed" || (rvm install $ruby_version && rvm --default use "$ruby_version")
 for gem in rails rails-api compass bundler; do
   [ -e $GEM_HOME/bin/$gem ] && echo $gem is already installed || gem install $gem
 done
